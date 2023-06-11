e138917208c22ef84133e7cd5c54760734db93c5
Junior Silva <zezinhojun@gmail.com>
2023-06-11 20:30:09 -0300

    feat: add component TransactionsTable

---
+++ src/components/TransactionsTable/index.tsx
@@ -0,0 +1,41 @@
+import { useEffect } from "react";
+import { Container } from "./styles";
+import { api } from "../../services/api";
+
+const TransactionsTable = () => {
+  useEffect(() => {
+    api.get("transactions")
+    .then((response) => console.log(response.data));
+  }, []);
+  return (
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <th>Título</th>
+            <th>Valor</th>
+            <th>Categoria</th>
+            <th>Data</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>Desenvolvimento de website</td>
+            <td className="deposit">R$2.000</td>
+            <td>Desenvolvimento</td>
+            <td>20/02/2022</td>
+          </tr>
+          <tr>
+            <td>Desenvolvimento de website</td>
+            <td className="withdraw">-R$1.000</td>
+            <td>Desenvolvimento</td>
+            <td>20/02/2022</td>
+          </tr>
+        </tbody>
+      </table>
+    </Container>
+  );
+};
+
+export default TransactionsTable;
