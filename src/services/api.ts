1e7da42875e9f66bc0c86698a8d101eff7acbd6f
Junior Silva <zezinhojun@gmail.com>
2024-02-05 13:10:01 -0300

    alterando urlvercel

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:3000/api"
+  baseURL: "dtmoney-b9jed96gq-junior-silvas-projects-d5dfed71.vercel.app/api"
 })
